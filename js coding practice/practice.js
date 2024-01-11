7907117faa104bb6729be532ee0579c6c05b4143
Prashant Kumar Singh <99783341+prashantkumarsingh07@users.noreply.github.com>
2024-01-11 20:45:42 +0530

    Add files via upload

---
+++ js coding practice/practice.js
@@ -0,0 +1,54 @@
+// variables
+
+// fullname="tony stark";
+// console.log(fullname);
+// x=null;
+// console.log(x);
+// isfollow=true;
+// console.log(isfollow);
+
+// fullname="prashant";
+// fullname="nishant";
+// console.log(fullname);
+
+// let,const & var
+
+// var age=23;
+// var age=67;
+// console.log(age);
+
+// let age=34;
+// age=45;
+// console.log(age);
+
+// const height=34;
+// {
+
+// }
+
+// DATA TYPES IN JS
+
+//  const student={
+//    fullname: "prashant",
+//    age:12,
+
+//  }
+// console.log(student.age);
+
+// const product={
+//   productName:"Parker-pen",
+//   rating:7200,
+//   offer:"5%" ,
+//   price:270,
+// }
+
+// console.log(product);
+// let b="123"+123;
+// console.log(b);
+
+// const profile={
+//   profileName:"sharadha",
+//   followers:569,
+// }
+
+// console.log(profile);
