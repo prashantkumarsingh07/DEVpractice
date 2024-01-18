afed6234ef56abfe6806f6c2d288fc22faf4e89f
Prashant Kumar Singh <99783341+prashantkumarsingh07@users.noreply.github.com>
2024-01-18 23:02:46 +0530

    Add files via upload

---
+++ countdown timer/index.js
@@ -0,0 +1,27 @@
+const endDate="1 January 2025 11:00 PM";
+document.getElementById("end-date").innerText=endDate;
+
+
+const inputs=document.querySelectorAll("input");
+
+function clock() {
+  const end=new Date(endDate);
+  
+  const now=new Date(); 
+  
+  const diff=(end-now)/1000;
+  // console.log(diff/3600/60/24);
+
+   inputs[0].value=Math.floor(diff/3600/24);
+   inputs[1].value=Math.floor(diff/3600) % 24;
+   inputs[2].value=Math.floor(diff/60) % 60;
+   inputs[3].value=Math.floor(diff) % 60;
+  
+
+ 
+
+}
+clock();
+setInterval(function(){
+ clock();
+},1000)
