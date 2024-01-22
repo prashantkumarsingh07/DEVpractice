013a275c35e5a266d6085e58f61b2f3311551c24
Prashant Kumar Singh <99783341+prashantkumarsingh07@users.noreply.github.com>
2024-01-22 23:58:10 +0530

    Add files via upload

--- Task_Planner_webapp/script.js
+++ Task_Planner_webapp/script.js
@@ -2,10 +2,11 @@ const inputbox=document.querySelector("input");
 const list=document.getElementById("list");
 const buttn=document.querySelector("button");
 const span=document.querySelector("span");
-
+//  const unchecked=document.querySelector("checked");
+//  const checked=document.querySelector(".checked");
 
 buttn.addEventListener("click",function(){
-  if(inputbox.value=="")
+  if(inputbox.value==" ")
   {
     alert("Add some task first");
   }
@@ -14,17 +15,39 @@ buttn.addEventListener("click",function(){
     let li=document.createElement("li");
     li.innerHTML=inputbox.value;
     list.appendChild(li);
-    let span=document.createElement("span");
-    span.innerHTML="\u00D7";
-    li.appendChild(span);
-    span.addEventListener("click",function(){
-      span.style.display="none";
-      li.style.display="none";
-    })
 
-  }
+     let span=document.createElement("span");
+     span.innerHTML="\u00D7";
+     li.appendChild(span);
+    // span.addEventListener("click",function(){
+    //   span.style.display="none";
+    //   li.style.display="none";
+    // })
+    // li.addEventListener("click",function(){
+    //   li.style.textDecoration="line-through";
+   
+     
+
+    }
+    inputbox.value=" ";
+    
+
+  
 
 })
+list.addEventListener("click",function(e){
+  if(e.target.tagName==="LI")
+  {
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagName==="SPAN")
+  {
+    e.target.parentElement.remove();
+  }
+
+},false);
+
+
 
 
 
